6474b3b0eb89bb4e5eac87c580d7aab70c79855b
Lauren Nicole Roth <laurenroth@gmail.com>
2016-01-04 17:52:33 -0500

    Add appointment adding alert box to Nearby controller

--- Cosset/NearbyViewController.swift
+++ Cosset/NearbyViewController.swift
@@ -7,13 +7,16 @@
 //
 
 import UIKit
+import Firebase
 
 class NearbyViewController: UITableViewController {
 
     var appointmentViewController: AppointmentViewController? = nil
-    var objects = [AnyObject]()
-
-
+    var appointments = [Appointment]()
+    let ref = Firebase(url: "https://cosset.firebaseio.com/")
+    let date = NSDate()
+    let formatter = NSDateFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -39,9 +42,30 @@ class NearbyViewController: UITableViewController {
     }
 
     func insertNewObject(sender: AnyObject) {
-        objects.insert(NSDate(), atIndex: 0)
-        let indexPath = NSIndexPath(forRow: 0, inSection: 0)
-        self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+        var alert = UIAlertController(title: "Appointment", message: "Add an appointment", preferredStyle: .Alert)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .Default) {
+            (action: UIAlertAction!) -> Void in
+            
+            let textField = alert.textFields![0] 
+            let appointment = Appointment(startTime: NSDate(), endTime: NSDate(), type: textField.text!, booked: true)
+            self.appointments.append(appointment)
+            self.tableView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Default) {
+            (action: UIAlertAction!) -> Void in
+        }
+        
+        alert.addTextFieldWithConfigurationHandler {
+            (textField: UITextField!) -> Void in
+        }
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        presentViewController(alert, animated: true, completion: nil)
+
     }
 
     // MARK: - Segues
@@ -49,9 +73,9 @@ class NearbyViewController: UITableViewController {
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "showDetail" {
             if let indexPath = self.tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate
+                let appointment = appointments[indexPath.row] as! NSDate
                 let controller = (segue.destinationViewController as! UINavigationController).topViewController as! AppointmentViewController
-                controller.detailItem = object
+                controller.detailItem = appointment
                 controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
                 controller.navigationItem.leftItemsSupplementBackButton = true
             }
@@ -65,16 +89,17 @@ class NearbyViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objects.count
+        return appointments.count
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
 
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+        let appointment = appointments[indexPath.row] 
+        cell.textLabel!.text = appointment.key
         return cell
     }
+
 
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
@@ -83,7 +108,7 @@ class NearbyViewController: UITableViewController {
 
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
-            objects.removeAtIndex(indexPath.row)
+            appointments.removeAtIndex(indexPath.row)
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
@@ -92,4 +117,3 @@ class NearbyViewController: UITableViewController {
 
 
 }
-
